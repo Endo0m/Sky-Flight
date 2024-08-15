8524cc76359917f8d020eb5d62644673868267d1
Endo0m <arthur.sadreev@gmail.com>
2024-08-15 17:48:31 +0300

    pushnotifications

---
+++ Assets/Scripts/PushNotificationsManager.cs
@@ -0,0 +1,22 @@
+using Firebase.Messaging;
+using UnityEngine;
+
+public class PushNotificationsManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseMessaging.TokenReceived += TokenReceived;
+            FirebaseMessaging.MessageReceived += MessageReceived;
+        });
+    }
+    private void TokenReceived(object sender, TokenReceivedEventArgs e)
+    {
+        Debug.Log("TokenReceived :" + e.Token);
+    }
+    private void MessageReceived(object sender, MessageReceivedEventArgs e)
+    {
+        Debug.Log("MessageReceived :" + e.Message);
+    }
+}
